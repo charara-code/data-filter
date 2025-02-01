c4329b0dfb3073f8f9e6d4139f995fdaab7a811b
Mohamad <m.charara@ob-soft.com>
2025-02-01 21:47:41 +0100

    Setup project

---
+++ src/data-filter/data_loader/base_data_loader.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+
+class BaseDataLoader(ABC):
+
+    def __init__(self, data_source):
+        self.data_source = data_source
+
+
+    @abstractmethod
+    def load_data(self):
+        pass
+
+    
